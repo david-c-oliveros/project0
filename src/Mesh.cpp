0a5876936e3850e4b82396fee35865b03208c35b
david-c-oliveros <david3oliveros@gmail.com>
2022-04-24 15:44:17 -0400

    Complete Mesh class

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -1,1 +1,100 @@
 #include "Mesh.h"
+
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures)
+{
+    this->vertices = vertices;
+    this->indices  = indices;
+    this->textures = textures;
+
+    setupMesh();
+}
+
+
+void Mesh::Draw(Shader &shader)
+{
+    unsigned int diffuseNr = 1;
+    unsigned int specularNr = 1;
+
+    for (unsigned int i = 0; i < textures.size(); i++)
+    {
+        glActiveTexture(GL_TEXTURE0 + i);
+        std::string number;
+        std::string name = textures[i].type;
+        if (name == "texture_diffuse")
+            number = std::to_string(diffuseNr++);
+        else if (name == "texture_specular")
+            number = std::to_string(specularNr++);
+
+        shader.SetFloat(("material." + name + number).c_str(), i);
+        glBindTexture(GL_TEXTURE_2D, textures[i].id);
+    }
+    glActiveTexture(GL_TEXTURE0);
+
+    /***************************/
+    /*        Draw Mesh        */
+    /***************************/
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+}
+
+
+void Mesh::setupMesh()
+{
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VAO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0]j, GL_STATIC_DRAW);
+
+    /**********************************/
+    /*        Vertex Positions        */
+    /**********************************/
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+
+    /********************************/
+    /*        Vertex Normals        */
+    /********************************/
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offset(Vertex, Normal));
+
+    /***************************************/
+    /*        Vertex Texture Coords        */
+    /***************************************/
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offset(Vertex, TexCoords));
+
+    /********************************/
+    /*        Vertex Tangent        */
+    /********************************/
+    glEnableVertexAttribArray(3);
+    glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offset(Vertex, Tangent));
+
+    /**********************************/
+    /*        Vertex Bitangent        */
+    /**********************************/
+    glEnableVertexAttribArray(4);
+    glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offset(Vertex, Bitangent));
+
+    /*********************/
+    /*        IDs        */
+    /*********************/
+    glEnableVertexAttribArray(5);
+    glVertexAttribPointer(5, 3, GL_INT, GL_FALSE, sizeof(Vertex), (void*)offset(Vertex, m_BoneIDs));
+
+    /*************************/
+    /*        Weights        */
+    /*************************/
+    glEnableVertexAttribArray(6);
+    glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offset(Vertex, m_Weights));
+
+    glBindVertexArray(0);
+}
